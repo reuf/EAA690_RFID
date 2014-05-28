dde2c7f5329a17d7869765d36b6fcedd06342168
Brian Michael <bsmichae42@yahoo.com>
2014-05-28 11:46:19 -0400

    Commenting and cleaning up

--- EAA690_Door/EAA690_Door.ino
+++ EAA690_Door/EAA690_Door.ino
@@ -1,4 +1,4 @@
-
+// Include libraries
 #include <SPI.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
@@ -6,74 +6,68 @@
 #include <SD.h>
 #include <Time.h>
 
+/********************
+ * GLOBAL VARIABLES *
+ ********************/
+
+// Network
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192,168,0,107);
+WiFiClient client;
+boolean wifiEnabled = true;
+boolean passRequired = false;
+char ssid[] = "EAA690"; // EAA690 your network SSID (name) // EAA690
+char pass[] = "PASSWORD";  // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+int status = WL_IDLE_STATUS;
+
+// LCD
 SoftwareSerial mySerial = SoftwareSerial(255, 3);
-char server[] = "www.brianmichael.org";
-String validcard = "554948485052706651505767";
+
+// SD Card
+boolean sdEnabled = true;
 char dbDelimiter = ',';
 String location = "hangar";
-IPAddress ip(192,168,0,107);
+
+char server[] = "www.brianmichael.org";
+String validcard = "554948485052706651505767";
+
+// Arduino PINs
 int LOCK = 2;
 int RFIDResetPin = 13;
 int GREEN = 8;
 int BLUE = 6;
 int RED = 5;
-WiFiClient client;
-boolean wifiEnabled = true;
-boolean sdEnabled = true;
-char ssid[] = "EAA690"; // EAA690 your network SSID (name) // EAA690
-boolean passRequired = false;
-char pass[] = "PASSWORD";  // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
+// NTP (time)
 unsigned long lastCheck = 0;
 unsigned long epoch = 0;
 long checkDelay = 600000;
-
-int status = WL_IDLE_STATUS;
 unsigned int localPort = 2390; // local port to listen for UDP packets
 IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server
 const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
 byte packetBuffer[NTP_PACKET_SIZE];
 WiFiUDP Udp;
 
+// Debug
+boolean debug = false;
+
+/************************************************
+ * Setup                                        *
+ *                                              *
+ * This is 1 of 2 required functions for all    *
+ * arduinos.  It is called upon startup (once)  *
+ * and initializes any variables and settings   *
+ ************************************************/
 void setup() {
   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
+  Serial.begin(9600);   // Init the computer console
+  mySerial.begin(9600); // Init the LCD
   
+  // Setup Arduino PINs
   pinMode(10, OUTPUT); // SD SS pin
   pinMode(3, OUTPUT);
   digitalWrite(3, HIGH);
-  //mySerial.begin(9600);
-  //displayLCD("Starting...", 2000, true);
-  
-  // check for the presence of the shield:
-  if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present"); 
-    // don't continue:
-    wifiEnabled = false;
-  } 
-  
-  // attempt to connect to Wifi network:
-  if (wifiEnabled) {
-    while (status != WL_CONNECTED) { 
-      String msg = "Connecting to   ";
-      msg.concat(ssid);
-      //displayLCD(msg, 1, true);
-      // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
-      if (passRequired) {
-        status = WiFi.begin(ssid, pass);
-      } else {
-        //displayLCD("Not passing password", 1, true);
-        status = WiFi.begin(ssid);
-      }
-  
-      // wait 1 second for connection:
-      delay(1000);
-    } 
-    //displayLCD("Connected to    wifi", 2000, true);    
-    Udp.begin(localPort);
-  }
-
   pinMode(RFIDResetPin, OUTPUT);
   pinMode(LOCK, OUTPUT);
   pinMode(GREEN, OUTPUT);
@@ -82,27 +76,40 @@ void setup() {
   digitalWrite(BLUE, HIGH);
   digitalWrite(RFIDResetPin, HIGH);
   
+  if (debug) displayLCD("Starting...", 2000, true);
+
+  // Init the network
+  initNetwork();
+  
+  // Check the SD card
   if (!SD.begin(4)) {
     sdEnabled = false;
   }
 }
 
+/************************************************
+ * Main Loop                                    *
+ *                                              *
+ * This is the 2nd required functions for all   *
+ * arduinos.  This function will be called      *
+ * repeatedly until the arduino is powered off  *
+ ************************************************/
 void loop() {
-  Serial.println("Begin loop");
-  String tagString = readTag();
-  unsigned long nowLong = now();
-  //Serial.print("time=");
-  //Serial.println(nowLong % 60);
+  if (debug) displayLCD("Begin loop", 50, false);
+    
   // Get the data once per hour, at the top of the hour
-  if ((nowLong % 86400) / 3600 == 0) {
+  unsigned long nowLong = now();
+  if ((nowLong % 86400) / 60 == 0) {
     getUserData();
   }
+
+  // Check to see if a tag needs to be checked
+  String tagString = readTag();
   if (tagString == "") {
     String timeStr = getTimeAsString();
     String msg = "Waiting...      " + timeStr;
     Serial.println(msg);
-    displayLCD(msg, 1, false);
-    delay(2000);
+    displayLCD(msg, 2000, false);
   } else {
     if (checkTag(tagString)) { //Check if it is a match
       openDoor(tagString);
@@ -113,42 +120,77 @@ void loop() {
   }
 }
 
-void getUserData() {
- //Serial.println(tag); //read out any unknown tag
- if (sdEnabled && client.connect(server, 80)) {
-   // Make a HTTP request:
-   String query = "GET /verify.php?rfid=-1";
-   query.concat(" HTTP/1.1");
-   Serial.println(query);
-   client.println(query);
-   String hostString = "Host: ";
-   hostString.concat(server);
-   client.println(hostString);
-   client.println("Connection: close");
-   client.println();
-   // Get HTTP response
-   delay(2000);
-   String response = "";
-   boolean bPre = false;
-   boolean buildingBPre = false;
-   String bPreStr = "";
-   boolean ePre = false;
-   boolean buildingEPre = false;
-   String ePreStr = "";
-   File dbFile = SD.open("database.csv", FILE_WRITE);
-   while (client.available()) { 
-     char c = client.read();
-     //Serial.print(c);
-     dbFile.print(c);
-   }
-   dbFile.close();
- } else {
-   // if you didn't get a connection to the server:
-   displayLCD("connection failed", 5000, true);
- }
- client.stop();
+/**
+ * Initialize the network so user and NTP data 
+ * can be transmitted/received
+ */
+void initNetwork() {
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    if (debug) displayLCD("WiFi shield not present", 1, false); 
+    // don't continue:
+    wifiEnabled = false;
+  } 
+  
+  // attempt to connect to Wifi network:
+  if (wifiEnabled) {
+    while (status != WL_CONNECTED) { 
+      String msg = "Connecting to   ";
+      msg.concat(ssid);
+      msg.concat('\0'); // Terminate string, may not be needed, but doesn't hurt
+      if (debug) displayLCD(msg, 1, true);
+      // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
+      if (passRequired) {
+        status = WiFi.begin(ssid, pass);
+      } else {
+        if (debug) displayLCD("Not passing password", 1, true);
+        status = WiFi.begin(ssid);
+      }
+  
+      // wait 1 second for connection:
+      delay(1000);
+    } 
+    if (debug) displayLCD("Connected to    wifi", 2000, true);    
+    Udp.begin(localPort);
+  }
 }
 
+/**
+ * Retrieve user database data (CSV) and store it 
+ * on the SD card.
+ */
+void getUserData() {
+  //Serial.println(tag); //read out any unknown tag
+  if (wifiEnabled && sdEnabled && client.connect(server, 80)) {
+    // Make a HTTP request:
+    String query = "GET /csv.php";
+    query.concat(" HTTP/1.1");
+    //Serial.println(query);
+    client.println(query);
+    String hostString = "Host: ";
+    hostString.concat(server);
+    client.println(hostString);
+    client.println("Connection: close");
+    client.println();
+    // Get HTTP response
+    displayLCD("Retrieving user data...", 2000, true);
+    File dbFile = SD.open("database.csv", FILE_WRITE);
+    while (client.available()) { 
+      char c = client.read();
+      //Serial.print(c);
+      dbFile.print(c);
+    }
+    dbFile.close();
+    client.stop();
+  } else {
+    // if you didn't get a connection to the server:
+    displayLCD("connection failed", 5000, true);
+  }
+}
+
+/**
+ * Reads the RFID from the card scanned
+ */
 String readTag() {
   String tagString;
   int index = 0;
@@ -166,13 +208,16 @@ String readTag() {
   return tagString;
 }
 
+/**
+ * Updates the arduino's "clock" with the current time from 
+ * time.nist.gov
+ */
 void updateTime() {
   unsigned long currentMillis = millis();
   if (epoch <= 0 || currentMillis - lastCheck > checkDelay) {
-    displayLCD("Updating time   from NTP...", 1, false);
     lastCheck = currentMillis;
     sendNTPpacket(timeServer);
-    delay(1000);
+    displayLCD("Updating time   from NTP...", 1000, false);
     if (Udp.parsePacket()) {
       Udp.read(packetBuffer, NTP_PACKET_SIZE);
       unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
@@ -186,6 +231,9 @@ void updateTime() {
   }
 }
 
+/**
+ * Formats the arduino's "clock" time to a human readable format
+ */
 String getTimeAsString() {
   updateTime();
   unsigned long nowLong = now();
@@ -205,6 +253,10 @@ String getTimeAsString() {
   return timeStr;
 }
 
+/**
+ * Helper method for updateTime().  
+ * Sends a UDP packet to time.nist.gov to get a NTP packet
+ */
 unsigned long sendNTPpacket(IPAddress& address) {
   memset(packetBuffer, 0, NTP_PACKET_SIZE);
   packetBuffer[0] = 0b11100011;
@@ -220,9 +272,9 @@ unsigned long sendNTPpacket(IPAddress& address) {
   Udp.endPacket();
 }
 
-///////////////////////////////////
-//Check the read tag against known tags
-///////////////////////////////////
+/**
+ * Check the read tag against known tags
+ */
 boolean checkTag(String tag) {
   boolean valid = false;
   if (tag == "") return false; //empty, no need to contunue
@@ -237,6 +289,9 @@ boolean checkTag(String tag) {
   return valid;
 }
 
+/**
+ * Retrieves the user entry from the SD card for the tag ID provided
+ */
 String getRecord(String tag) {
   String returnLine;
   if (sdEnabled) {
@@ -250,8 +305,8 @@ String getRecord(String tag) {
           String id = getValue(line, dbDelimiter,  0);
           //String firstName = getValue(line, dbDelimiter,  1);
           //String lastName = getValue(line, dbDelimiter,  2);
-          String date = getValue(line, dbDelimiter,  3);
-          String access = getValue(line, dbDelimiter,  4);
+          //String date = getValue(line, dbDelimiter,  3);
+          //String access = getValue(line, dbDelimiter,  4);
           //String message = getValue(line, dbDelimiter,  5);
           //Serial.println("id=["+id+"]; firstName=["+firstName+"]; lastName=["+lastName+"]; date=["+date+"]; access=["+access+"]; message=["+message+"]");
           if (id == tag) {
@@ -269,6 +324,10 @@ String getRecord(String tag) {
   return returnLine;
 }
 
+/**
+ * Helper routine for getRecord, and other functions
+ * Parses a line, and returns the value at the specified location.
+ */
 String getValue(String data, char separator, int index) {
   int found = 0;
   int strIndex[] = {0, -1};
@@ -285,68 +344,80 @@ String getValue(String data, char separator, int index) {
   return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
+/**
+ * Compares dates to determine access validity
+ */
 boolean checkDate(String dateToCheck) {
   boolean ok = true;
   // TODO
   return ok;
 }
 
-///////////////////////////////////
-//Reset the RFID reader to read again.
-///////////////////////////////////
+/**
+ * Reset the RFID reader to read again.
+ */
 void resetReader(){
   digitalWrite(RFIDResetPin, LOW);
   digitalWrite(RFIDResetPin, HIGH);
   delay(150);
 }
 
+/**
+ * Handles the "access denied" state
+ */
 void accessDenied(String id) {
   String record = getRecord(id);
   String firstName = getValue(record, dbDelimiter, 1);
   String lastName = getValue(record, dbDelimiter, 2);
   String message = getValue(record, dbDelimiter, 5);
-  Serial.print("Access Denied for ");
-  Serial.print(firstName);
-  Serial.print(" ");
-  Serial.print(lastName);
-  Serial.print(" ");
-  Serial.println(message);
+  //Serial.print("Access Denied for ");
+  //Serial.print(firstName);
+  //Serial.print(" ");
+  //Serial.print(lastName);
+  //Serial.print(" ");
+  //Serial.println(message);
   digitalWrite(BLUE, LOW);
   digitalWrite(RED, HIGH);
-  displayLCD("Access Denied", 5000, true);
+  displayLCD(message, 5000, true);
   digitalWrite(RED, LOW);
   digitalWrite(BLUE, HIGH);
 }
 
+/**
+ * Handles the "access granted" state
+ */
 void openDoor(String id) {
   String record = getRecord(id);
   String firstName = getValue(record, dbDelimiter, 1);
   String lastName = getValue(record, dbDelimiter, 2);
   String message = getValue(record, dbDelimiter, 5);
-  Serial.print("Opening door for ");
-  Serial.print(firstName);
-  Serial.print(" ");
-  Serial.println(lastName);
+  //Serial.print("Opening door for ");
+  //Serial.print(firstName);
+  //Serial.print(" ");
+  //Serial.println(lastName);
   digitalWrite(BLUE, LOW);
   digitalWrite(GREEN, HIGH);
   digitalWrite(LOCK, HIGH);
   displayLCD(message, 5000, true);
-  Serial.println("Closing door");
+  //Serial.println("Closing door");
   displayLCD("Closing door", 5000, true);
   digitalWrite(LOCK, LOW);
   digitalWrite(GREEN, LOW);
   digitalWrite(BLUE, HIGH);
 }
 
+/**
+ * Prints a message to the LCD screen for a specified length of time
+ * Effectively a log method
+ */
 void displayLCD(String msg, int d, boolean backlight) {
   mySerial.write(12);    // Clear             
   if (backlight) {
     mySerial.write(17);  // Turn backlight on
   }
-  delay(5);              // Required delay
   mySerial.print(msg);   // message
+  delay(d);              // Wait "d" seconds
   if (backlight) {
-    delay(d);            // Wait "d" seconds
     mySerial.write(18);  // Turn backlight off
   }
 }
