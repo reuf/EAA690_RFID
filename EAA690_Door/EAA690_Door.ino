d70df7fe257c316bb74c5dae1fb6805f871b3863
bsmichael <bsmichae42@yahoo.com>
2015-03-20 19:21:31 -0400

    data transfers

--- EAA690_Door/EAA690_Door.ino
+++ EAA690_Door/EAA690_Door.ino
@@ -47,6 +47,7 @@
 */
 
 #include <Wire.h>
+#include <EasyTransferI2C.h>
 
 /********************
  * GLOBAL VARIABLES *
@@ -69,7 +70,28 @@ int COLOR_MODE_ORANGE = 6;
 int COLOR_MODE_WHITE = 7;
 int COLOR_MODE_OFF = 0;
 
-String tag;
+EasyTransferI2C ET;
+
+struct DATA_STRUCTURE {
+  int access = 0;
+  int door = I2C;
+  char char1;
+  char char2;
+  char char3;
+  char char4;
+  char char5;
+  char char6;
+  char char7;
+  char char8;
+  char char9;
+  char char10;
+  char char11;
+  char char12;
+};
+
+DATA_STRUCTURE tagData;
+
+String tag = "";
 
 /************************************************
  * Setup                                        *
@@ -86,8 +108,8 @@ void setup() {
   digitalWrite(RFIDResetPin, HIGH);
   
   Wire.begin(I2C);
-  Wire.onRequest(requestEvent); // register event
-  Wire.onReceive(receiveEvent); // register event
+  ET.begin(details(tagData), &Wire);
+  Wire.onReceive(receive);
 
   Serial.begin(9600);  // set up Serial library at 9600 bps
   
@@ -104,18 +126,44 @@ void setup() {
 void loop() {    
   char val = 0; // variable to store the data from the serial port
 
+  int count = 0;
   // Check to see if a tag needs to be checked
   if (tag == "") {
     while (Serial.available() > 0) {
       val = Serial.read();
-      tag = tag + val;
+      if (count == 1) tagData.char1 = val;
+      if (count == 2) tagData.char2 = val;
+      if (count == 3) tagData.char3 = val;
+      if (count == 4) tagData.char4 = val;
+      if (count == 5) tagData.char5 = val;
+      if (count == 6) tagData.char6 = val;
+      if (count == 7) tagData.char7 = val;
+      if (count == 8) tagData.char8 = val;
+      if (count == 9) tagData.char9 = val;
+      if (count == 10) tagData.char10 = val;
+      if (count == 11) tagData.char11 = val;
+      if (count == 11) tagData.char12 = val;
+      if (count > 0) tag += val;
+      count++;
     }
     if (tag != "") {
-      tag.replace("\n", "");
-      tag = tag.substring(1, tag.length() - 1);
+      //tagData.tag.replace("\n", "");
+      //tagData.tag = tagData.tag.substring(1, tagData.tag.length() - 1);
+      //Serial.println("Read tag " + tagData.tag);
+      ET.sendData(0);
     }
   }
   
+  if (ET.receiveData()) {
+    if (tagData.access > 0) {
+      openDoor();
+    } else {
+      accessDenied();
+    }
+    tag = "";
+    tagData.access = 0;
+  }
+
   //reset the RFID reader
   digitalWrite(RFIDResetPin, LOW);
   digitalWrite(RFIDResetPin, HIGH);
@@ -125,31 +173,7 @@ void loop() {
 }
 
 // Called by Main Controller to let us know if access is granted.
-void receiveEvent(int howMany) {
-  if (Wire.read() == 'Y') {
-    openDoor();
-  } else {
-    accessDenied();
-  }
-  tag = "";
-}
-
-// Called by Main Controller to see if a card has been read.
-void requestEvent() {
-  if (tag != "") {
-    Wire.write(charArray(tag));
-  } else {
-    Wire.write("0");
-  }
-}
-
-// Helper method to convert a String to a char array
-char* charArray(String str) {
-  int strLength = sizeof(str);
-  char strArray[strLength];
-  str.toCharArray(strArray, strLength);
-  return strArray;
-}
+void receive(int howMany) {}
 
 /**
  * Handles the "access denied" state
