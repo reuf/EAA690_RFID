173e8545cdc254d6808c54e3c4b54c5a3452de42
Brian Michael <bmichael@brians-mbp.launchmodem.com>
2015-03-30 15:30:12 -0400

    addUsage() changes

--- addUsage.php
+++ addUsage.php
@@ -1,9 +1,9 @@
 <?php
-   	include(“database.php");
+   	include("database.php");
    	
    	$link=Connection();
 
-	$entry=$_POST[“entry”];
+	$entry=$_POST["entry"];
         $entryArray = explode(',', $entry);
 
         if (sizeof($entryArray) == 4) {
@@ -11,7 +11,7 @@
           //INSERT INTO  `eaa690`.`door_usage` (`time` ,`tag` ,`door` ,`access`) VALUES ('2015-03-30 03:23:11',  '23FR4576DF34',  '1',  '1');
 
     	  $query = "INSERT INTO `door_usage` (`time`, `tag`, `door`, `access`) 
-	    	  VALUES ('".$entryArray[0].”’,’”.$entryArray[1].”’,’”.$entryArray[2].”’,’”.$entryArray[3].”’)”; 
+	    	  VALUES ('".$entryArray[0]."','".$entryArray[1]."','".$entryArray[2]."','".$entryArray[3]."')"; 
    	
    	  mysql_query($query,$link);
 	  mysql_close($link);
